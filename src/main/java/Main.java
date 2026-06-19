dc9d10023cfa3440cd40e6cdad41c70c50ecc035
yashvardhan25bcs10761-dot <yashvardhan.25bcs10761@sst.scaler.com>
2026-06-20 02:19:25 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,14 +6,18 @@ public class Main {
     static List<String> parse(String s) {
         List<String> res = new ArrayList<>();
         StringBuilder cur = new StringBuilder();
-        boolean q = false;
+
+        boolean sq = false;
+        boolean dq = false;
 
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
 
-            if (c == '\'') {
-                q = !q;
-            } else if (Character.isWhitespace(c) && !q) {
+            if (c == '\'' && !dq) {
+                sq = !sq;
+            } else if (c == '"' && !sq) {
+                dq = !dq;
+            } else if (Character.isWhitespace(c) && !sq && !dq) {
                 if (cur.length() > 0) {
                     res.add(cur.toString());
                     cur.setLength(0);
