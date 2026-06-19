84aaa1124041ae7b2b89a575fd38bd0fa2aaa0f1
yashvardhan25bcs10761-dot <yashvardhan.25bcs10761@sst.scaler.com>
2026-06-20 00:43:29 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,12 @@
-import java.util.Scanner;
-import java.util.List;
+import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         List<String> builtins = List.of("echo", "exit", "type");
+        String pathEnv = System.getenv("PATH");
+        String[] paths = pathEnv != null ? pathEnv.split(File.pathSeparator) : new String[0];
         
         while (true) {
             System.out.print("$ ");
@@ -16,7 +18,19 @@ public class Main {
                 System.out.println(input.substring(5));
             } else if (input.startsWith("type ")) {
                 String cmd = input.substring(5);
-                System.out.println(builtins.contains(cmd) ? cmd + " is a shell builtin" : cmd + ": not found");
+                if (builtins.contains(cmd)) {
+                    System.out.println(cmd + " is a shell builtin");
+                } else {
+                    String foundPath = null;
+                    for (String p : paths) {
+                        File f = new File(p, cmd);
+                        if (f.exists() && f.canExecute()) {
+                            foundPath = f.getAbsolutePath();
+                            break;
+                        }
+                    }
+                    System.out.println(foundPath != null ? cmd + " is " + foundPath : cmd + ": not found");
+                }
             } else {
                 System.out.println(input + ": command not found");
             }
