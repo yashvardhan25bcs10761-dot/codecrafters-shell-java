bfaefebd90421c9e09955a709ebaaaae121e7a7f
yashvardhan25bcs10761-dot <yashvardhan.25bcs10761@sst.scaler.com>
2026-06-20 00:16:01 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-         //     TODO: Uncomment the code below to pass the first stage
-         System.out.print("$ ");
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("$ ");
+
+            String input = scanner.nextLine();
+
+            System.out.println(input + ": command not found");
+        }
     }
 }
