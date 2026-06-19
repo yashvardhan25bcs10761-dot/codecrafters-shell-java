fdc95dba29bfb541319a6e77919bb8ee45dfc49e
yashvardhan25bcs10761-dot <yashvardhan.25bcs10761@sst.scaler.com>
2026-06-20 00:24:07 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,9 +8,13 @@ Scanner scanner = new Scanner(System.in);
             System.out.print("$ ");
             String input = scanner.nextLine();
             
-            if (input.equals("exit")) break;
-            
-            System.out.println(input + ": command not found");
+            if (input.equals("exit")) {
+                break;
+            } else if (input.startsWith("echo ")) {
+                System.out.println(input.substring(5));
+            } else {
+                System.out.println(input + ": command not found");
+            }
         }
     }
 }
