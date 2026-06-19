6535a3215956e37cde8b94de8ce02fe08e554825
yashvardhan25bcs10761-dot <yashvardhan.25bcs10761@sst.scaler.com>
2026-06-20 00:23:03 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,15 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-Scanner scanner = new Scanner(System.util.in);
+Scanner scanner = new Scanner(System.in);
         
         while (true) {
             System.out.print("$ ");
             String input = scanner.nextLine();
             
-            if (input.equals("exit")) {
-                break;
-            }
+            if (input.equals("exit")) break;
             
             System.out.println(input + ": command not found");
         }
